274c529f4bd7af8ea7fcc16c7e0221f662a66f5c
Aliaksandr <relon@tut.by>
2018-02-01 15:57:37 +0300

    Set cam2web library and use for local camera

---
+++ HomeAutomationWebServer/Controllers/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeAutomationWebServer.Controllers
+{
+    public class CameraController : Controller
+    {
+        // GET: Camera
+        public ActionResult Index()
+        {
+            return View();
+            //return Redirect("http://10.0.43.54:8000/");
+        }
+        // GET: IpCameras
+        public ActionResult IpCameras()
+        {
+            //check if there is Ip camera then 
+            return View();
+            //if there is not IP camera then go to Info page
+        }
+    }
+}
